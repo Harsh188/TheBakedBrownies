6f8a1919c89993e941e243c502b501621ab043f8
Harsh188 <hiharshith18@gmail.com>
2022-02-12 12:55:02 +0530

    added huroku url

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import axios from 'axios';
+
 
 function App() {
+	const [getMessage, setGetMessage] = useState({})
+
+	useEffect(()=>{
+    	axios.get('https://the-baked-brownies.herokuapp.com/flask/hello').then(response => {
+      		console.log("SUCCESS", response)
+      		setGetMessage(response)
+    	}).catch(error => {
+      		console.log(error)
+    	})
+  	}, [])
+
     return {
         <>
-        <div className = "App">
-            
-        </div>
+        	<div className = "App">
+        	</div>
         </>
     };
 }
